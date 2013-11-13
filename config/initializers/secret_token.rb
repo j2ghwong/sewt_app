7435b0fd0036f74c6f6ad9b6581bc10568441ac9
Joseph Wong <josephwong@macosx01.interface23.com>
2013-11-13 18:25:27 -0500

    Initial release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SewtApp::Application.config.secret_token = 'dbcd9e920422e37f1fe2b17f08501e2862e0db5c6d9ccbe14fec6d24ec933c630e413f41b67acceef479d1dd75c87e90d67c4dfda8453bc7c9ecbaaf2d3745f8'
